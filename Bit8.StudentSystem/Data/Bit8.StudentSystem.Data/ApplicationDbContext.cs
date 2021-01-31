ef69582784b9f4df7a9a270bbc0dea03ffb523d9
playboxtech_andrej <andrej@playboxtech.com>
2021-01-31 17:59:15 +0200

    Db creation, drop and creation of tables

--- Bit8.StudentSystem/Data/Bit8.StudentSystem.Data/ApplicationDbContext.cs
+++ Bit8.StudentSystem/Data/Bit8.StudentSystem.Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 using Bit8.StudentSystem.Data.Interfaces;
 
@@ -9,6 +10,7 @@ namespace Bit8.StudentSystem.Data
     public class ApplicationDbContext : IApplicationDbContext
     {
         private readonly string connectionString;
+        private const string BasePath = @"..\..\Data\Bit8.StudentSystem.Data\SQLs\";
 
         public ApplicationDbContext(string connectionString)
         {
@@ -17,13 +19,56 @@ namespace Bit8.StudentSystem.Data
 
         public void Initialize()
         {
-            var connection = new MySqlConnection(this.connectionString);
-            connection.Open();
+            this.DropDbIfExistsAndRecreate();
+            this.CreateTables();
 
-            var stm = "SELECT * FROM world.country LIMIT 1";
-            var cmd = new MySqlCommand(stm, connection);
+        }
 
-            var version = cmd.ExecuteScalar().ToString();
+        private void CreateTables()
+        {
+            var statement = string.Empty;
+            var path = $"{BasePath}CreateTables.txt";
+
+            if (File.Exists(path))
+            {
+                statement = File.ReadAllText(path);
+            }
+
+            using (var connection = new MySqlConnection(this.connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand(statement, connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        private void DropDbIfExistsAndRecreate()
+        {
+            var dbName = this.GetDatabaseName(this.connectionString);
+            var masterConnectionString = this.connectionString.Replace($"Database={dbName};", string.Empty);
+
+            using (var connection = new MySqlConnection(masterConnectionString))
+            {
+                connection.Open();
+
+                var statement = $"DROP SCHEMA IF EXISTS {dbName};";
+                var command = new MySqlCommand(statement, connection);
+                command.ExecuteNonQuery();
+
+                statement = $"CREATE DATABASE IF NOT EXISTS {dbName};";
+                command = new MySqlCommand(statement, connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        private string GetDatabaseName(string connectionString)
+        {
+            var databaseString = "Database=";
+            var indexOfDbNameStart = connectionString.IndexOf(databaseString) + databaseString.Length;
+            var indexOfDbNameEnd = connectionString.IndexOf(';', indexOfDbNameStart);
+            return connectionString.Substring(indexOfDbNameStart, indexOfDbNameEnd - indexOfDbNameStart);
         }
     }
 }
